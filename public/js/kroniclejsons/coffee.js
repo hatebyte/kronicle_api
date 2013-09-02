d78791c50605bc0073f0a5f3d6047e8d95f86939
hatebyte <hatebyte@gmail.com>
2013-09-02 11:48:37 -0700

    Added example kronicles jsons

---
+++ public/js/kroniclejsons/coffee.js
@@ -0,0 +1,82 @@
+jQuery.post('http://166.78.151.97:4711/kronicles/', {
+  "title": "Press Pot Coffee",
+  "description": "Using a Press Pot (aka French Press) is the easiest and best way to get truly excellent coffee at home. The keys to getting good results are: using high quality, fresh beans; grinding the coffee correctly; using clean equipment and filtered water; timing the process.",
+  "category": "food",
+  "imageUrl": "coffee-cover.png",
+  "items" : [
+    "Press Pot", 
+    "coffee",
+    "a grinder",
+    "a spoon",
+    "cups (and thermal carafe if preparing more than fits in the cups)"],
+  "steps": [
+    {
+      "title": "1. Grind Coffee",
+      "description": "It is important that the coffee be ground coarse and that it be ground with a quality burr (rather than blade) grinder. By grinding the coffee coarse, you’re allowing for a slower and more even extraction, which results in a fuller bodied and more nuanced cup. Blade grinders chop the coffee rather than grinding it, resulting in uneven and unpredictable particle size. This leads to an uneven extraction, creating increased bitterness. In addition, the lack of consistency in particle size results in inconsistent and unpredictable results from pot to pot.",
+      "imageUrl": "coffee-img-part-1.png",
+      "time": 120,
+      "indexInKronicle": 0
+    },
+    {
+      "title": "2.Boil Water",
+      "description": "You should bring the water just to a boil (electric kettles are great at this).",
+      "imageUrl": "coffee-img-part-2.png",
+      "time": 600,
+      "indexInKronicle": 1
+    },
+    {
+      "title": "3.Add Coffee to the Pot",
+      "description": "You’ll need one tablespoon (7 grams) of coffee for every 4 oz of water. in other words, if you have a 34 oz (8 cup) Press Pot, you’ll want to use 8 tablespoons of coffee. Feel free to adjust this amount based on your own personal tastes. Make sure the pot is clean and dry.",
+      "imageUrl": "coffee-img-part-3.png",
+      "time": 45,
+      "indexInKronicle": 2
+    },
+    {
+      "title": "4.Add Water",
+      "description": "Pour it aggressively into the pot so that it saturates the grounds. The key is to saturate all the grounds evenly. You should move the stream around as you pour to facilitate this. Do not fill the pot entirely.\n\nWith many fresh coffees you will see significant expansion of the coffee in a sort of foam at the top of the liquid once you add water. This is known as bloom and is the result of the off-gassing of CO2 from the coffee. Adding too much water can result in a very messy countertop.",
+      "imageUrl": "coffee-img-part-4.png",
+      "time": 60,
+      "indexInKronicle": 3
+    },
+    {
+      "title": "5.Stir and Finish Steeping",
+      "description": "You should stir the grounds in the pot. If you need to add water to top off the pot, make sure it is right below boiling. Stirring the pot guarantees even and optimal extraction of all the coffee. In addition, it breaks down the bloom and allows you to combine the correct amount of water and coffee without spilling all over the place.",
+      "imageUrl": "coffee-img-part-5.png",
+      "time": 180,
+      "indexInKronicle": 4
+    },
+    {
+      "title": "6. Press the Pot",
+      "description": "You should push the press (slowly) into the pot to force all grounds to the bottom. You might have to press and then release and repeat to do this. Do not crush it with all your might – use some finesse.",
+      "imageUrl": "coffee-img-part-6.png",
+      "time": 45,
+      "indexInKronicle": 5
+    },
+    {
+      "title": "7. Pour the Coffee",
+      "description": "You need to do this as soon as you’ve pressed the pot. If you’re making more coffee than you can fit into a cup and want to hold some for later, pour the coffee into a thermal carafe. Do not simply leave the coffee in the Press Pot — it will get nasty quickly. If you want to avoid any stray grounds and sediment, you can pour the coffee through a mesh basket filter.",
+      "imageUrl": "coffee-img-part-7.png",
+      "time": 60,
+      "indexInKronicle": 6
+    }
+  ]
+}, response);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/js/kroniclejsons/yoga.js
@@ -0,0 +1,55 @@
+jQuery.post('http://166.78.151.97:4711/kronicles/', {
+  "title": "Fat Fighting Yoga",
+  "description": "Do this routine 4 times per week, drink lots of water, eat healthy, and you will be on your way to your ideal weight and happiness state.",
+  "category": "exercise",
+  "imageUrl": "yoga-cover.png",
+  "items" : [
+    "Yoga Mat",
+    "Comfortable non-restrictive clothing"
+  ],
+  "steps": [
+    {
+      "title": "1. Upward Salute",
+      "description": "Urdhva Hastasana literally translates to 'Raised Hands Pose,' but it is also sometimes called Talasana, the Palm Tree Pose.",
+      "imageUrl": "yoga-vid-step-1.mov",
+      "time": 60,
+      "indexInKronicle": 0
+    },
+    {
+      "title": "2. Chair Pose",
+      "description": "Chair Pose clearly works the muscles of the arms and legs, but it also stimulates the diaphragm and heart.",
+      "imageUrl": "yoga-vid-step-2.mov",
+      "time": 60,
+      "indexInKronicle": 1
+    },
+    {
+      "title": "3. Locust Pose",
+      "description": "An effective means for strengthening the back of the torso, legs, and arms in preparation for the deeper backbends.",
+      "imageUrl": "yoga-vid-step-3.mov",
+      "time": 60,
+      "indexInKronicle": 2
+    },
+    {
+      "title": "4. knee push up",
+      "description": "'Modified' or 'knee' push-ups are performed by supporting the lower body on the knees instead of the toes, which reduces the difficulty. This is useful for warm ups/downs, pyramids/drop sets, endurance training and rehab. ",
+      "imageUrl": "yoga-vid-step-4.mov",
+      "time": 60,
+      "indexInKronicle": 3
+    },
+    {
+      "title": "5. Upward-Facing Dog",
+      "description": "Upward-Facing Dog will challenge you to lift and open your chest.",
+      "imageUrl": "yoga-vid-step-5.mov",
+      "time": 60,
+      "indexInKronicle": 4
+    },
+    {
+      "title": "6. Upward Salute",
+      "description": "Urdhva Hastasana literally translates to 'Raised Hands Pose,' but it is also sometimes called Talasana, the Palm Tree Pose.",
+      "imageUrl": "yoga-vid-step-6.mov",
+      "time": 60,
+      "indexInKronicle": 5
+    }
+  
+  ]
+}, response);
