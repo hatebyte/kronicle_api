00f3acaf883ad69740abcf91a75474020426076d
hatebyte <hatebyte@gmail.com>
2013-09-02 11:06:57 -0700

    Added cross domain

--- server.js
+++ server.js
@@ -39,12 +39,18 @@ app.configure(function() {
 	app.use(express.static(path.join(__dirname, "public")));
 });
 
+
 app.configure("development", function() {
 	app.use(express.errorHandler({ dummpExceptions:true, showStack:true }));
 	//app.use('/user', user);
 	app.use('/kronicles', require('./node_modules/kronicle/kronicles'));
 });
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 app.listen(4711, function() {
 	console.log("Express server listening on port");
